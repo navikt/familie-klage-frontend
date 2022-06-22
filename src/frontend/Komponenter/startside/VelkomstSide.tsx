d498ca7366947e9f43bef3904686f1d4c477a168
Viktor <viktor.grondalen.solberg@nav.no>
2022-06-22 17:37:00 +0200

    skal kunne hente tekst fra testController i familie-klage

--- src/frontend/Komponenter/startside/VelkomstSide.tsx
+++ src/frontend/Komponenter/startside/VelkomstSide.tsx
@@ -3,27 +3,36 @@ import { Side } from '../../Felles/Visningskomponenter/Side';
 import { Ressurs, RessursStatus } from '../../App/typer/ressurs';
 import { useApp } from '../../App/context/AppContext';
 import { Normaltekst } from 'nav-frontend-typografi';
+import styled from 'styled-components';
+
+const Visningstekst = styled(Normaltekst)`
+    text-align: center;
+    text-transform: uppercase;
+    color: #4caf50;
+    font-size: 5rem;
+    margin: 4rem;
+`;
 
 export const VelkomstSide: React.FC = () => {
     const { axiosRequest } = useApp();
-    const [velkomstTekst, settVelkomstTekst] = useState<string>('Ikke koblet til backend');
+    const [visningstekst, settVisningstekst] = useState<string>('Fikk ikke kontakt med backend');
 
     useEffect(() => {
         document.title = 'Oppgavebenk';
-        axiosRequest<null, null>({
+        axiosRequest<string, null>({
             method: 'GET',
-            url: `/familie-ef-klage/api/test`,
-        }).then((res: Ressurs<null>) => {
+            url: `/familie-klage/api/test`,
+        }).then((res: Ressurs<string>) => {
             if (res.status === RessursStatus.SUKSESS) {
-                settVelkomstTekst('Koblet til backend');
+                settVisningstekst(res.data);
             }
         });
     }, []);
 
     return (
         <Side className={'container'}>
-            <Normaltekst>Hello world</Normaltekst>
-            <Normaltekst>{velkomstTekst}</Normaltekst>
+            <Visningstekst>Hello world</Visningstekst>
+            <Visningstekst>{visningstekst}</Visningstekst>
         </Side>
     );
 };
