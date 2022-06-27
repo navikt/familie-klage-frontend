6505afefec531659f2cfe4dfdf0d26cfdf43d2c1
Henrik Moe Wæhre <henrikmw@live.no>
2022-06-27 09:09:12 +0200

    Fikset bug på å skrive inn tekst i textarea.

--- src/frontend/Komponenter/Behandling/Formkrav/Formkrav.tsx
+++ src/frontend/Komponenter/Behandling/Formkrav/Formkrav.tsx
@@ -11,79 +11,82 @@ import {
     Button,
 } from '@navikt/ds-react';
 
+const FormKravStyling = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 90%;
+    margin: 0 5% 0 5%;
+`;
+
+const FormKravStylingHeader = styled.div`
+    width: 100%;
+    display: flex;
+    padding: 5% 5% 0 5%;
+    flex-direction: column;
+`;
+
+const FormKravStylingBody = styled.div`
+    display: flex;
+    @media only screen and (max-width: 800px) {
+        flex-direction: column;
+    }
+    width: 100%;
+`;
+
+const FormKravStylingFooter = styled.div`
+    width: 100%;
+    display: flex;
+    padding-left: 5%;
+`;
+
+const FormKravStylingVenstre = styled.div`
+    width: 50%;
+    padding: 2% 5% 5% 5%;
+`;
+
+const FormKravStylingHøyre = styled.div`
+    width: 50%;
+    padding: 2% 5% 5% 5%;
+`;
+
+const RadioKnapperContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const RadioKnapperRadContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    padding-top: 2%;
+`;
+
+const RadioGroupKnapperContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    padding-right: 2%;
+`;
+
+const RadioStyled = styled(Radio)`
+    padding: 2%;
+`;
+
+const AlertStyled = styled(Alert)`
+    width: 100%;
+`;
+const RadioGroupStyled = styled(RadioGroup)`
+    padding: 2% 5%% 0 0;
+    width: 40%;
+`;
+
+const TextareaStyled = styled(Textarea)``;
+
 export const Formkrav: React.FC = () => {
-    const [value, setValue] = useState('');
-    const [klagePart, setKlagePart] = useState('');
-
-    const FormKravStyling = styled.div`
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        width: 90%;
-        margin: 0 5% 0 5%;
-    `;
-
-    const FormKravStylingHeader = styled.div`
-        width: 100%;
-        display: flex;
-        padding: 5% 5% 0 5%;
-        flex-direction: column;
-    `;
-
-    const FormKravStylingBody = styled.div`
-        display: flex;
-        @media only screen and (max-width: 800px) {
-            flex-direction: column;
-        }
-        width: 100%;
-    `;
-
-    const FormKravStylingFooter = styled.div`
-        width: 100%;
-        display: flex;
-        padding-left: 5%;
-    `;
-
-    const FormKravStylingVenstre = styled.div`
-        width: 50%;
-        padding: 2% 5% 5% 5%;
-    `;
-
-    const FormKravStylingHøyre = styled.div`
-        width: 50%;
-        padding: 2% 5% 5% 5%;
-    `;
-
-    const RadioKnapperContainer = styled.div`
-        display: flex;
-        flex-direction: column;
-    `;
-
-    const RadioKnapperRadContainer = styled.div`
-        display: flex;
-        flex-direction: row;
-        padding-top: 2%;
-    `;
-
-    const RadioGroupKnapperContainer = styled.div`
-        display: flex;
-        flex-direction: row;
-        padding-right: 2%;
-    `;
-
-    const RadioStyled = styled(Radio)`
-        padding: 2%;
-    `;
-
-    const AlertStyled = styled(Alert)`
-        width: 100%;
-    `;
-    const RadioGroupStyled = styled(RadioGroup)`
-        padding: 2% 5%% 0 0;
-        width: 40%;
-    `;
-
-    const TextareaStyled = styled(Textarea)``;
+    const [vurdering, settVurdering] = useState('');
+    const [klagePart, settKlagePart] = useState();
+    const [klageKonkret, settKlageKonkret] = useState();
+    const [klageFrist, settKlageFrist] = useState();
+    const [klageSignert, settKlageSignert] = useState();
 
     return (
         <FormKravStyling>
@@ -101,15 +104,16 @@ export const Formkrav: React.FC = () => {
             <FormKravStylingBody>
                 <FormKravStylingVenstre>
                     <Textarea
-                        value={value}
-                        onChange={(e) => setValue(e.target.value)}
+                        label={undefined}
+                        value={vurdering}
+                        onChange={(e) => settVurdering(e.target.value)}
                         size="small"
                         description="Vurdering"
                         maxLength={1500}
                     />
                 </FormKravStylingVenstre>
                 <FormKravStylingHøyre>
-                    <Select size="small" description="Vedtaket som er påklagd">
+                    <Select size="small" description="Vedtaket som er påklagd" label={undefined}>
                         <option value="">Velg vedtak</option>
                         <option value="norge">Norge</option>
                         <option value="sverige">Sverige</option>
@@ -120,21 +124,21 @@ export const Formkrav: React.FC = () => {
                             <RadioGroupStyled
                                 legend="Er klager part i saken?"
                                 size="small"
-                                onChange={(val: any) => setKlagePart('')}
+                                onChange={(val: any) => settKlagePart(val)}
                             >
                                 <RadioGroupKnapperContainer>
-                                    <RadioStyled value="10">Ja</RadioStyled>
-                                    <RadioStyled value="20">Nei</RadioStyled>
+                                    <RadioStyled value="Ja">Ja</RadioStyled>
+                                    <RadioStyled value="Nei">Nei</RadioStyled>
                                 </RadioGroupKnapperContainer>
                             </RadioGroupStyled>
                             <RadioGroupStyled
                                 legend="Klages det på konkrete elementer i vedtaket?"
                                 size="small"
-                                onChange={(val: any) => setKlagePart('')}
+                                onChange={(val: any) => settKlageKonkret(val)}
                             >
                                 <RadioGroupKnapperContainer>
-                                    <RadioStyled value="10">Ja</RadioStyled>
-                                    <RadioStyled value="20">Nei</RadioStyled>
+                                    <RadioStyled value="Ja">Ja</RadioStyled>
+                                    <RadioStyled value="Nei">Nei</RadioStyled>
                                 </RadioGroupKnapperContainer>
                             </RadioGroupStyled>
                         </RadioKnapperRadContainer>
@@ -142,21 +146,21 @@ export const Formkrav: React.FC = () => {
                             <RadioGroupStyled
                                 legend="Er klagefristen overholdt?"
                                 size="small"
-                                onChange={(val: any) => setKlagePart('')}
+                                onChange={(val: any) => settKlageFrist(val)}
                             >
                                 <RadioGroupKnapperContainer>
-                                    <RadioStyled value="10">Ja</RadioStyled>
-                                    <RadioStyled value="20">Nei</RadioStyled>
+                                    <RadioStyled value="Ja">Ja</RadioStyled>
+                                    <RadioStyled value="Nei">Nei</RadioStyled>
                                 </RadioGroupKnapperContainer>
                             </RadioGroupStyled>
                             <RadioGroupStyled
                                 legend="Er klagen signert?"
                                 size="small"
-                                onChange={(val: any) => setKlagePart('')}
+                                onChange={(val: any) => settKlageSignert(val)}
                             >
                                 <RadioGroupKnapperContainer>
-                                    <RadioStyled value="10">Ja</RadioStyled>
-                                    <RadioStyled value="20">Nei</RadioStyled>
+                                    <RadioStyled value="Ja">Ja</RadioStyled>
+                                    <RadioStyled value="Nei">Nei</RadioStyled>
                                 </RadioGroupKnapperContainer>
                             </RadioGroupStyled>
                         </RadioKnapperRadContainer>
