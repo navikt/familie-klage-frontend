08e435634b2fcae728f3e550a6adeff063f4436e
junileirvik <juni.leirvik@gmail.com>
2022-06-28 12:58:58 +0200

    Fjernet etiketter og ubrukt kode fra visittkort.

--- src/frontend/Felles/Visittkort/Visittkort.tsx
+++ src/frontend/Felles/Visittkort/Visittkort.tsx
@@ -3,32 +3,18 @@ import { IPersonopplysninger } from '../../App/typer/personopplysninger';
 import Visittkort from '@navikt/familie-visittkort';
 import styled from 'styled-components';
 import { Element } from 'nav-frontend-typografi';
-import PersonStatusVarsel from '../Varsel/PersonStatusVarsel';
-import AdressebeskyttelseVarsel from '../Varsel/AdressebeskyttelseVarsel';
-import { EtikettFokus, EtikettInfo, EtikettSuksess } from 'nav-frontend-etiketter';
 import { Behandling } from '../../App/typer/fagsak';
 import navFarger from 'nav-frontend-core';
 import { Sticky } from '../Visningskomponenter/Sticky';
-import { erEtterDagensDato } from '../../App/utils/dato';
 import { RessursFeilet, RessursStatus, RessursSuksess } from '../../App/typer/ressurs';
 import { useApp } from '../../App/context/AppContext';
 import { ISøkPerson } from '../../App/typer/personsøk';
 import Lenke from 'nav-frontend-lenker';
 import { IPersonIdent } from '../../App/typer/felles';
-import Alertstripe from 'nav-frontend-alertstriper';
+//import Alertstripe from 'nav-frontend-alertstriper';
 import { Hamburgermeny } from './Hamburgermeny';
 import { erBehandlingRedigerbar } from '../../App/typer/behandlingstatus';
-import {
-    behandlingstypeTilTekst,
-    behandlingstypeTilTekstKort,
-} from '../../App/typer/behandlingstype';
 import { AlleStatuser, StatuserLitenSkjerm, StatusMeny } from './Status/StatusElementer';
-import {
-    Stønadstype,
-    stønadstypeTilTekst,
-    stønadstypeTilTekstKort,
-} from '../../App/typer/behandlingstema';
-import { Behandlingsårsak, behandlingsårsakTilTekst } from '../../App/typer/Behandlingsårsak';
 
 const Visningsnavn = styled(Element)`
     text-overflow: ellipsis;
@@ -40,28 +26,6 @@ const ResponsivLenke = styled(Lenke)`
     text-overflow: ellipsis;
     overflow: hidden;
     white-space: nowrap;
-`;
-
-const TagsKnyttetTilBehandling = styled.div`
-    display: flex;
-    justify-content: flex-end;
-    flex-grow: 1;
-`;
-
-const TagsLitenSkjerm = styled.div`
-    @media screen and (min-width: 946px) {
-        display: none;
-    }
-
-    @media screen and (max-width: 760px) {
-        display: none;
-    }
-`;
-
-const TagsStorSkjerm = styled.div`
-    @media screen and (max-width: 946px) {
-        display: none;
-    }
 `;
 
 export const VisittkortWrapper = styled(Sticky)`
@@ -85,41 +49,14 @@ const StyledHamburgermeny = styled(Hamburgermeny)`
     z-index: 9999;
 `;
 
-const ElementWrapper = styled.div`
-    margin-left: 1rem;
-`;
-
 const VisittkortComponent: FC<{ data: IPersonopplysninger; behandling?: Behandling }> = ({
     data,
     behandling,
 }) => {
-    const {
-        personIdent,
-        kjønn,
-        navn,
-        folkeregisterpersonstatus,
-        adressebeskyttelse,
-        egenAnsatt,
-        fullmakt,
-        vergemål,
-    } = data;
+    const { personIdent, kjønn, navn } = data;
 
     const { axiosRequest, gåTilUrl } = useApp();
     const [fagsakPersonId, settFagsakPersonId] = useState<string>('');
-    const [erMigrert, settErMigrert] = useState(false);
-    const [feilFagsakHenting, settFeilFagsakHenting] = useState<string>();
-
-    const utledOmFagsakErMigrert = (fagsak: {
-        fagsakId: string;
-        stønadstype: Stønadstype;
-        erLøpende: boolean;
-        erMigrert: boolean;
-    }) => {
-        if (behandling) {
-            return behandling.fagsakId === fagsak.fagsakId && fagsak.erMigrert;
-        }
-        return fagsak.stønadstype === Stønadstype.OVERGANGSSTØNAD && fagsak.erMigrert;
-    };
 
     useEffect(() => {
         const hentFagsak = (personIdent: string): void => {
@@ -133,10 +70,7 @@ const VisittkortComponent: FC<{ data: IPersonopplysninger; behandling?: Behandli
                 if (respons.status === RessursStatus.SUKSESS) {
                     if (respons.data?.fagsakPersonId) {
                         settFagsakPersonId(respons.data.fagsakPersonId);
-                        settErMigrert(respons.data.fagsaker.some(utledOmFagsakErMigrert));
                     }
-                } else {
-                    settFeilFagsakHenting(respons.frontendFeilmelding);
                 }
             });
         };
@@ -165,78 +99,7 @@ const VisittkortComponent: FC<{ data: IPersonopplysninger; behandling?: Behandli
                         <Visningsnavn>{navn.visningsnavn}</Visningsnavn>
                     </ResponsivLenke>
                 }
-            >
-                {folkeregisterpersonstatus && (
-                    <ElementWrapper>
-                        <PersonStatusVarsel folkeregisterpersonstatus={folkeregisterpersonstatus} />
-                    </ElementWrapper>
-                )}
-                {adressebeskyttelse && (
-                    <ElementWrapper>
-                        <AdressebeskyttelseVarsel adressebeskyttelse={adressebeskyttelse} />
-                    </ElementWrapper>
-                )}
-                {egenAnsatt && (
-                    <ElementWrapper>
-                        <EtikettFokus mini>Egen ansatt</EtikettFokus>
-                    </ElementWrapper>
-                )}
-                {fullmakt.some((f) => erEtterDagensDato(f.gyldigTilOgMed)) && (
-                    <ElementWrapper>
-                        <EtikettFokus mini>Fullmakt</EtikettFokus>
-                    </ElementWrapper>
-                )}
-
-                {vergemål.length > 0 && (
-                    <ElementWrapper>
-                        <EtikettFokus mini>Verge</EtikettFokus>
-                    </ElementWrapper>
-                )}
-
-                {erMigrert && (
-                    <ElementWrapper>
-                        <EtikettFokus mini>Migrert</EtikettFokus>
-                    </ElementWrapper>
-                )}
-
-                <TagsKnyttetTilBehandling>
-                    {behandling && (
-                        <ElementWrapper>
-                            <TagsLitenSkjerm>
-                                <EtikettSuksess mini>
-                                    {stønadstypeTilTekstKort[behandling.stønadstype]}
-                                </EtikettSuksess>
-                            </TagsLitenSkjerm>
-                            <TagsStorSkjerm>
-                                <EtikettSuksess mini>
-                                    {stønadstypeTilTekst[behandling.stønadstype]}
-                                </EtikettSuksess>
-                            </TagsStorSkjerm>
-                        </ElementWrapper>
-                    )}
-                    {behandling && (
-                        <ElementWrapper>
-                            <TagsLitenSkjerm>
-                                <EtikettInfo mini>
-                                    {behandlingstypeTilTekstKort[behandling.type]}
-                                </EtikettInfo>
-                            </TagsLitenSkjerm>
-                            <TagsStorSkjerm>
-                                <EtikettInfo mini>
-                                    {behandlingstypeTilTekst[behandling.type]}
-                                </EtikettInfo>
-                            </TagsStorSkjerm>
-                        </ElementWrapper>
-                    )}
-                    {behandling && behandling.behandlingsårsak === Behandlingsårsak.PAPIRSØKNAD && (
-                        <ElementWrapper>
-                            <EtikettFokus mini>
-                                {behandlingsårsakTilTekst[behandling.behandlingsårsak]}
-                            </EtikettFokus>
-                        </ElementWrapper>
-                    )}
-                </TagsKnyttetTilBehandling>
-            </Visittkort>
+            ></Visittkort>
 
             {behandling && (
                 <>
