6ae5cc9ac1138fc0cd61f7ba154ecf9df1a85e1c
Henrik Moe Wæhre <henrikmw@live.no>
2022-07-06 13:02:14 +0200

    Fiks testside, fanemeny wip

--- src/frontend/Komponenter/Behandling/Fanemeny/Fanemeny.tsx
+++ src/frontend/Komponenter/Behandling/Fanemeny/Fanemeny.tsx
@@ -1,22 +1,18 @@
 import * as React from 'react';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import styled from 'styled-components';
 import { alleSider } from './sider';
 import DataViewer from '../../../Felles/DataViewer/DataViewer';
 import { Sticky } from '../../../Felles/Visningskomponenter/Sticky';
 import navFarger from 'nav-frontend-core';
 import Fane from './Fane';
-import { behandlingMock } from '../BehandlingContainer';
+import { hentBehandlingIdFraUrl } from '../BehandlingContainer';
 import { RessursStatus } from '../../../App/typer/ressurs';
+import { useHentBehandling } from '../../../App/hooks/useHentBehandling';
 
 const StickyMedBoxShadow = styled(Sticky)`
     box-shadow: 0 2px 5px rgba(0, 0, 0, 0.4);
 `;
-
-const mock = {
-    data: behandlingMock,
-    status: RessursStatus.SUKSESS,
-};
 
 const StyledFanemeny = styled.div`
     width: 100%;
@@ -34,6 +30,16 @@ interface Props {
 const Fanemeny: FC<Props> = ({ behandlingId }) => {
     // const { behandling } = useBehandling();
     const forrigeSideLagret = false; //Sier hvilke sider som skal være mulige å redigere
+    const { behandling, hentBehandlingCallback } = useHentBehandling(hentBehandlingIdFraUrl());
+
+    useEffect(() => {
+        hentBehandlingCallback();
+    });
+
+    const mock = {
+        data: behandling,
+        status: RessursStatus,
+    };
 
     return (
         <DataViewer response={{ mock }}>
