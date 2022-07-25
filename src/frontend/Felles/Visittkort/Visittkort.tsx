ce00c1656e823db640f076b3c7cea1b37d21255e
maja <maja_ying@hotmail.com>
2022-07-25 11:03:49 +0200

    fjernet feilmelding ved første innhenting av formkrav

--- src/frontend/Felles/Visittkort/Visittkort.tsx
+++ src/frontend/Felles/Visittkort/Visittkort.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC } from 'react';
 import { IPersonopplysninger } from '../../App/typer/personopplysninger';
 import Visittkort from '@navikt/familie-visittkort';
 import styled from 'styled-components';
@@ -6,22 +6,11 @@ import { Element } from 'nav-frontend-typografi';
 import { Behandling } from '../../App/typer/fagsak';
 import navFarger from 'nav-frontend-core';
 import { Sticky } from '../Visningskomponenter/Sticky';
-import { RessursFeilet, RessursStatus, RessursSuksess } from '../../App/typer/ressurs';
-import { useApp } from '../../App/context/AppContext';
-import { ISøkPerson } from '../../App/typer/personsøk';
-import Lenke from 'nav-frontend-lenker';
-import { IPersonIdent } from '../../App/typer/felles';
 import { Hamburgermeny } from './Hamburgermeny';
 import { erBehandlingRedigerbar } from '../../App/typer/behandlingstatus';
 import { AlleStatuser, StatuserLitenSkjerm, StatusMeny } from './Status/StatusElementer';
 
 const Visningsnavn = styled(Element)`
-    text-overflow: ellipsis;
-    overflow: hidden;
-    white-space: nowrap;
-`;
-
-const ResponsivLenke = styled(Lenke)`
     text-overflow: ellipsis;
     overflow: hidden;
     white-space: nowrap;
@@ -53,52 +42,13 @@ const VisittkortComponent: FC<{
     behandling: Behandling;
 }> = ({ personopplysninger, behandling }) => {
     const { personId, kjønn, navn } = personopplysninger;
-
-    const { id } = behandling;
-
-    const { axiosRequest, gåTilUrl } = useApp();
-    const [fagsakPersonId, settFagsakPersonId] = useState<string>('');
-
-    useEffect(() => {
-        const hentFagsak = (id: string): void => {
-            if (!id) return;
-
-            axiosRequest<ISøkPerson, IPersonIdent>({
-                method: 'POST',
-                url: `/familie-klage/api/sok/`,
-                data: { personIdent: personId },
-            }).then((respons: RessursSuksess<ISøkPerson> | RessursFeilet) => {
-                if (respons.status === RessursStatus.SUKSESS) {
-                    if (respons.data?.fagsakPersonId) {
-                        settFagsakPersonId(respons.data.fagsakPersonId);
-                    }
-                }
-            });
-        };
-
-        hentFagsak(id);
-
-        // eslint-disable-next-line
-    }, []);
-
     return (
         <VisittkortWrapper>
             <Visittkort
                 alder={20}
                 ident={personId}
                 kjønn={kjønn}
-                navn={
-                    <ResponsivLenke
-                        role={'link'}
-                        href={`/person/${fagsakPersonId}`}
-                        onClick={(e) => {
-                            e.preventDefault();
-                            gåTilUrl(`/person/${fagsakPersonId}`);
-                        }}
-                    >
-                        <Visningsnavn>{navn}</Visningsnavn>
-                    </ResponsivLenke>
-                }
+                navn={<Visningsnavn>{navn}</Visningsnavn>}
             ></Visittkort>
 
             {behandling && (
