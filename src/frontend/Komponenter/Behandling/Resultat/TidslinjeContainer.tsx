02968dcfc7171393543d58d995035f6b2b8bf43d
Henrik Moe Wæhre <henrikmw@live.no>
2022-07-27 09:28:11 +0200

    Fiks konsollfeil

--- src/frontend/Komponenter/Behandling/Resultat/TidslinjeContainer.tsx
+++ src/frontend/Komponenter/Behandling/Resultat/TidslinjeContainer.tsx
@@ -4,6 +4,7 @@ import { IBehandlingshistorikk } from '../Høyremeny/behandlingshistorikk';
 import { Tidslinje } from './Tidslinje';
 import { StegType, StegTypeListe } from '../../../App/typer/fagsak';
 import { fullførteSteg, StegHistorikkResultat, finnBehandlinghistorikkFraListe } from './utils';
+import { v4 as uuidv4 } from 'uuid';
 
 export const TidslinjeContainer: React.FC<{
     historikkForVisning: IBehandlingshistorikk[];
@@ -15,9 +16,10 @@ export const TidslinjeContainer: React.FC<{
     return (
         <div>
             {StegTypeListe.map((item, index) => (
-                <>
+                <div key={uuidv4()}>
                     {fullført.includes(item) ? (
                         <Tidslinje
+                            key={uuidv4()}
                             steg={item}
                             behandlingHistorikk={finnBehandlinghistorikkFraListe(
                                 historikkForVisning,
@@ -28,12 +30,13 @@ export const TidslinjeContainer: React.FC<{
                         />
                     ) : (
                         <Tidslinje
+                            key={uuidv4()}
                             steg={item}
                             førsteObjekt={index == 0}
                             aktivtSteg={aktivtSteg == item}
                         />
                     )}
-                </>
+                </div>
             ))}
         </div>
     );
