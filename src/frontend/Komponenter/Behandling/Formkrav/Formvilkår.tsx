996db12b352497625786b1ec67b24908979bf1bb
junileirvik <juni.leirvik@gmail.com>
2022-08-01 15:30:38 +0200

    Endret litt på magrin

--- src/frontend/Komponenter/Behandling/Formkrav/Formvilkår.tsx
+++ src/frontend/Komponenter/Behandling/Formkrav/Formvilkår.tsx
@@ -12,7 +12,7 @@ const VilkårStyling = styled.div`
     flex-direction: column;
     align-items: center;
     width: 50%;
-    margin: 0.5rem 1rem 1 rem 1rem;
+    margin: 0.5rem 1rem 1 rem 0rem;
 `;
 
 const FormKravStylingBody = styled.div`
@@ -32,7 +32,7 @@ const RadioKnapperContainer = styled.div`
 
 const RadioGroupStyled = styled(RadioGroup)`
     padding: 0.1rem 0;
-    margin: 0.8rem 0;
+    margin: 0.5rem 0;
     width: 100%;
 `;
 
