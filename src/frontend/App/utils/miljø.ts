d66665b9260d21339109e5146a75a2a7ac57a172
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2023-05-04 15:21:17 +0200

    Sletter ubrukt fil

--- src/frontend/App/utils/miljø.ts
+++
@@ -1,6 +0,0 @@
-export const erProd = (): boolean => window.location.host === 'familie-klage.intern.nav.no';
-
-export const tilbakekrevingBaseUrl = (): string =>
-    erProd()
-        ? 'https://familietilbakekreving.intern.nav.no'
-        : 'https://familie-tilbake-frontend.intern.dev.nav.no';
