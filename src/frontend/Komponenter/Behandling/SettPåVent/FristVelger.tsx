d43449d5de7a89ba1dd324c4d78d59a2be8ad6dd
krikof-nav <kristian.kofoed@nav.no>
2024-12-19 12:04:51 +0100

    Endret FamilieSelect og FristVelger til å bruke readOnly argument. Feltet vil nå låse seg når vi er i lesemodus.

--- src/frontend/Komponenter/Behandling/SettPåVent/FristVelger.tsx
+++ src/frontend/Komponenter/Behandling/SettPåVent/FristVelger.tsx
@@ -1,7 +1,6 @@
 import React, { FC } from 'react';
-import { useDatepicker, DatePicker, Label, BodyShort } from '@navikt/ds-react';
+import { useDatepicker, DatePicker } from '@navikt/ds-react';
 import { nullableTilDato, tilLocaleDateString } from '../../../App/utils/dato';
-import { formaterNullableIsoDato } from '../../../App/utils/formatter';
 import { IOppgave } from './IOppgave';
 
 export const FristVelger: FC<{
@@ -14,21 +13,16 @@ export const FristVelger: FC<{
         onDateChange: (dato) => settFrist(dato && tilLocaleDateString(dato)),
     });
 
-    const formatertFristDato = formaterNullableIsoDato(oppgave.fristFerdigstillelse);
     const dagensDato = new Date();
 
     return (
-        <div>
-            {erLesevisning ? (
-                <div>
-                    <Label size="small">Frist</Label>
-                    <BodyShort size="small">{formatertFristDato}</BodyShort>
-                </div>
-            ) : (
-                <DatePicker {...datepickerProps} fromDate={dagensDato}>
-                    <DatePicker.Input label={'Frist'} {...inputProps} size={'small'} />
-                </DatePicker>
-            )}
-        </div>
+        <DatePicker {...datepickerProps} fromDate={dagensDato}>
+            <DatePicker.Input
+                label={'Frist'}
+                {...inputProps}
+                size={'small'}
+                readOnly={erLesevisning}
+            />
+        </DatePicker>
     );
 };
