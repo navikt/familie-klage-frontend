b7d764b556eed3be248ee63484767145515a8198
thoalm <thoalm@outlook.com>
2024-12-19 15:03:08 +0100

    Rydder litt opp i "RegistrerteBrevmottakere"

--- src/frontend/Komponenter/Behandling/Brevmottakere/baks/modal/oversikt/RegistrerteBrevmottakere.tsx
+++ src/frontend/Komponenter/Behandling/Brevmottakere/baks/modal/oversikt/RegistrerteBrevmottakere.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { TrashIcon } from '@navikt/aksel-icons';
-import { Alert, Button, Heading, HStack, VStack } from '@navikt/ds-react';
+import { Alert, Box, Button, Heading, HStack, VStack } from '@navikt/ds-react';
 import { AFontWeightBold } from '@navikt/ds-tokens/dist/tokens';
 import CountryData from '@navikt/land-verktoy';
 import { Brevmottaker } from '../../brevmottaker';
@@ -36,53 +36,59 @@ export function RegistrerteBrevmottakere({
     slettBrevmottaker,
     erLesevisning,
 }: Props) {
-    return brevmottakere.map((brevmottaker) => {
-        const land = CountryData.getCountryInstance('nb').findByValue(brevmottaker.landkode);
-        return (
-            <VStack key={brevmottaker.id} marginBlock={'2 2'}>
-                <HStack justify={'space-between'}>
-                    <Heading level={'2'} size={'medium'}>
-                        {mottakertypeVisningsnavn[brevmottaker.mottakertype]}
-                    </Heading>
-                    {!erLesevisning && (
-                        <Button
-                            variant={'tertiary'}
-                            onClick={() => slettBrevmottaker(brevmottaker.id)}
-                            size={'small'}
-                            icon={<TrashIcon />}
-                        >
-                            Fjern
-                        </Button>
-                    )}
-                </HStack>
-                <DefinitionList>
-                    <dt>Navn</dt>
-                    <dd>{brevmottaker.navn}</dd>
-                    <dt>Land</dt>
-                    <dd>{land.label}</dd>
-                    <dt>Adresselinje 1</dt>
-                    <dd>{brevmottaker.adresselinje1}</dd>
-                    <dt>Adresselinje 2</dt>
-                    <dd>{brevmottaker.adresselinje2 || '-'}</dd>
-                    {brevmottaker.postnummer && (
-                        <>
-                            <dt>Postnummer</dt>
-                            <dd>{brevmottaker.postnummer || '-'}</dd>
-                        </>
-                    )}
-                    {brevmottaker.poststed && (
-                        <>
-                            <dt>Poststed</dt>
-                            <dd>{brevmottaker.poststed || '-'}</dd>
-                        </>
-                    )}
-                </DefinitionList>
-                {brevmottaker.landkode !== EøsLandkode.NO && (
-                    <Alert variant={'info'} inline={true}>
-                        Ved utenlandsk adresse skal postnummer og poststed legges i adresselinjene.
-                    </Alert>
-                )}
-            </VStack>
-        );
-    });
+    const countryInstance = CountryData.getCountryInstance('nb');
+    return (
+        <Box padding={'1'}>
+            {brevmottakere.map((brevmottaker) => {
+                const land = countryInstance.findByValue(brevmottaker.landkode).label;
+                return (
+                    <VStack key={brevmottaker.id} marginBlock={'2 2'}>
+                        <HStack justify={'space-between'}>
+                            <Heading level={'2'} size={'small'}>
+                                {mottakertypeVisningsnavn[brevmottaker.mottakertype]}
+                            </Heading>
+                            {!erLesevisning && (
+                                <Button
+                                    variant={'tertiary'}
+                                    onClick={() => slettBrevmottaker(brevmottaker.id)}
+                                    size={'small'}
+                                    icon={<TrashIcon />}
+                                >
+                                    Fjern
+                                </Button>
+                            )}
+                        </HStack>
+                        <DefinitionList>
+                            <dt>Navn</dt>
+                            <dd>{brevmottaker.navn}</dd>
+                            <dt>Land</dt>
+                            <dd>{land}</dd>
+                            <dt>Adresselinje 1</dt>
+                            <dd>{brevmottaker.adresselinje1}</dd>
+                            <dt>Adresselinje 2</dt>
+                            <dd>{brevmottaker.adresselinje2 || '-'}</dd>
+                            {brevmottaker.postnummer && (
+                                <>
+                                    <dt>Postnummer</dt>
+                                    <dd>{brevmottaker.postnummer || '-'}</dd>
+                                </>
+                            )}
+                            {brevmottaker.poststed && (
+                                <>
+                                    <dt>Poststed</dt>
+                                    <dd>{brevmottaker.poststed || '-'}</dd>
+                                </>
+                            )}
+                        </DefinitionList>
+                        {brevmottaker.landkode !== EøsLandkode.NO && (
+                            <Alert variant={'info'} inline={true}>
+                                Ved utenlandsk adresse skal postnummer og poststed legges i
+                                adresselinjene.
+                            </Alert>
+                        )}
+                    </VStack>
+                );
+            })}
+        </Box>
+    );
 }
