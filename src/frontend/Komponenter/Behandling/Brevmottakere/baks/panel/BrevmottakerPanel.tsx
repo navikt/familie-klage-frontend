16966f9c740149cc12114a33f31439a189844d85
thoalm <thoalm@outlook.com>
2024-12-19 11:44:16 +0100

    Fjerner unødvendig InfoHeader styled component

--- src/frontend/Komponenter/Behandling/Brevmottakere/baks/panel/BrevmottakerPanel.tsx
+++ src/frontend/Komponenter/Behandling/Brevmottakere/baks/panel/BrevmottakerPanel.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
 import { useApp } from '../../../../../App/context/AppContext';
 import { useBehandling } from '../../../../../App/context/BehandlingContext';
-import { Alert, BodyShort, Button, Label, Tooltip } from '@navikt/ds-react';
-import styled from 'styled-components';
+import { BodyShort, Box, Button, HStack, Label, Tooltip } from '@navikt/ds-react';
 import { Brevmottaker, utledOppsumeringsnavnPåBrevmottakere } from '../brevmottaker';
-
-const InfoHeader = styled.div`
-    display: grid;
-    grid-template-columns: 26rem 12rem;
-`;
 
 type Props = {
     brevmottakere: Brevmottaker[];
@@ -21,27 +15,27 @@ export function BrevmottakerPanel({ brevmottakere }: Props) {
     const navn = utledOppsumeringsnavnPåBrevmottakere(brevmottakere);
 
     return (
-        <Alert variant={'info'}>
-            <InfoHeader>
-                <Label>Brevmottakere:</Label>
+        <Box padding={'10'} background={'surface-info-subtle'}>
+            <HStack justify={'space-between'} align={'center'}>
+                <Label htmlFor={'brevmottakere_liste'}>Brevmottakere</Label>
                 {behandlingErRedigerbar && (
-                    <Tooltip content={'Legg til verge eller fullmektige brevmottakere'}>
+                    <Tooltip content={'Legg til eller fjern brevmottakere'}>
                         <Button
                             variant={'tertiary'}
                             onClick={() => settVisBrevmottakereModal(true)}
                         >
-                            Legg til/endre brevmottakere
+                            Legg til/fjern brevmottakere
                         </Button>
                     </Tooltip>
                 )}
-            </InfoHeader>
-            <ul>
+            </HStack>
+            <ul id={'brevmottakere_liste'}>
                 {navn.map((navn, index) => (
                     <li key={navn + index}>
                         <BodyShort key={navn + index}>{navn}</BodyShort>
                     </li>
                 ))}
             </ul>
-        </Alert>
+        </Box>
     );
 }
