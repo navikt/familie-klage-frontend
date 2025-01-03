f728ab787effd8fb8fdef63ff376c4daeceaa1e6
krikof-nav <kristian.kofoed@nav.no>
2025-01-03 13:31:00 +0100

    Fikset SettPåVentKnapp slik at den faktisk fungerer som forventet. Den viser nå sett på vent innhold når knappen blir trykket på.

--- src/frontend/Felles/Visittkort/SettPåVentKnapp.tsx
+++ src/frontend/Felles/Visittkort/SettPåVentKnapp.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { Button } from '@navikt/ds-react';
 import { useToggles } from '../../App/context/TogglesContext';
 import { ToggleName } from '../../App/context/toggles';
+import { useBehandling } from '../../App/context/BehandlingContext';
 
 export const SettPåVentKnapp = () => {
+    const { settVisSettPåVent, behandlingErRedigerbar } = useBehandling();
     const { toggles } = useToggles();
 
     const visSettPåVent = toggles[ToggleName.visSettPåVent];
@@ -13,7 +15,12 @@ export const SettPåVentKnapp = () => {
     }
 
     return (
-        <Button size="xsmall" variant="secondary">
+        <Button
+            disabled={!behandlingErRedigerbar}
+            onClick={() => settVisSettPåVent(true)}
+            size="xsmall"
+            variant="secondary"
+        >
             Sett på vent
         </Button>
     );
