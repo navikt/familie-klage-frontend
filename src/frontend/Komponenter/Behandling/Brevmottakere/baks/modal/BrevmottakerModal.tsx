9b332b775a842170cce27b1e81deaf130c069175
thoalm <thoalm@outlook.com>
2025-01-03 11:26:29 +0100

    Splittet opp Modal i en egen Body og Footer komponent

--- src/frontend/Komponenter/Behandling/Brevmottakere/baks/modal/BrevmottakerModal.tsx
+++ src/frontend/Komponenter/Behandling/Brevmottakere/baks/modal/BrevmottakerModal.tsx
@@ -1,15 +1,11 @@
-import React, { useState } from 'react';
-
-import { PlusCircleIcon } from '@navikt/aksel-icons';
-import { Alert, Button, Modal, VStack } from '@navikt/ds-react';
-
-import { RegistrerteBrevmottakere } from './oversikt/RegistrerteBrevmottakere';
+import React from 'react';
+import { Modal } from '@navikt/ds-react';
 import { useApp } from '../../../../../App/context/AppContext';
-import { BrevmottakerForm } from './form/BrevmottakerForm';
 import { IPersonopplysninger } from '../../../../../App/typer/personopplysninger';
-import { Brevmottaker, finnesBrevmottakerMedMottakertype } from '../brevmottaker';
-import { Mottakertype } from '../mottakertype';
+import { Brevmottaker } from '../brevmottaker';
 import { OpprettBrevmottakerDto } from '../opprettBrevmottakerDto';
+import { BrevmottakerModalBody } from './BrevmottakerModalBody';
+import { BrevmottakerModalFooter } from './BrevmottakerModalFooter';
 
 type Props = {
     behandlingId: string;
@@ -39,80 +35,27 @@ export function BrevmottakerModal({
     erLesevisning,
 }: Props) {
     const { visBrevmottakereModal, settVisBrevmottakereModal } = useApp();
-    const [visForm, settVisForm] = useState(false);
-
-    const finnesBrevmottakerMedDødsbo = finnesBrevmottakerMedMottakertype(
-        brevmottakere,
-        Mottakertype.DØDSBO
-    );
-
-    const erFormSynlig =
-        !erLesevisning &&
-        (brevmottakere.length === 0 ||
-            (brevmottakere.length === 1 && !finnesBrevmottakerMedDødsbo && visForm));
-
-    const erLeggTilNyMottakerKnappSynlig =
-        !erLesevisning &&
-        !erFormSynlig &&
-        !finnesBrevmottakerMedDødsbo &&
-        brevmottakere.length === 1;
-
-    function lukkModal() {
-        settVisBrevmottakereModal(false);
-        settVisForm(false);
-    }
-
     return (
         <Modal
             open={visBrevmottakereModal}
-            onClose={lukkModal}
+            onClose={() => settVisBrevmottakereModal(false)}
             header={{ heading: utledHeading(brevmottakere.length, erLesevisning), size: 'medium' }}
             width={'40rem'}
             portal={true}
         >
-            <Modal.Body>
-                {visBrevmottakereModal && (
-                    <VStack gap={'4'}>
-                        <Alert variant={'info'}>
-                            Brev sendes til brukers folkeregistrerte adresse eller annen foretrukken
-                            kanal. Legg til mottaker dersom brev skal sendes til utenlandsk adresse,
-                            fullmektig, verge eller dødsbo.
-                        </Alert>
-                        <RegistrerteBrevmottakere
-                            brevmottakere={brevmottakere}
-                            slettBrevmottaker={slettBrevmottaker}
-                            erLesevisning={erLesevisning}
-                        />
-                        {erFormSynlig && (
-                            <BrevmottakerForm
-                                behandlingId={behandlingId}
-                                personopplysninger={personopplysninger}
-                                brevmottakere={brevmottakere}
-                                erLesevisning={false}
-                                lukkModal={lukkModal}
-                                opprettBrevmottaker={opprettBrevmottaker}
-                            />
-                        )}
-                        {erLeggTilNyMottakerKnappSynlig && (
-                            <div>
-                                <Button
-                                    variant={'tertiary'}
-                                    size={'small'}
-                                    icon={<PlusCircleIcon />}
-                                    onClick={() => settVisForm(true)}
-                                >
-                                    Legg til ny mottaker
-                                </Button>
-                            </div>
-                        )}
-                    </VStack>
-                )}
-            </Modal.Body>
-            <Modal.Footer>
-                <Button variant={'tertiary'} size={'medium'} onClick={lukkModal}>
-                    Lukk vindu
-                </Button>
-            </Modal.Footer>
+            {visBrevmottakereModal && (
+                <>
+                    <BrevmottakerModalBody
+                        behandlingId={behandlingId}
+                        personopplysninger={personopplysninger}
+                        brevmottakere={brevmottakere}
+                        opprettBrevmottaker={opprettBrevmottaker}
+                        slettBrevmottaker={slettBrevmottaker}
+                        erLesevisning={erLesevisning}
+                    />
+                    <BrevmottakerModalFooter />
+                </>
+            )}
         </Modal>
     );
 }
---
+++ src/frontend/Komponenter/Behandling/Brevmottakere/baks/modal/BrevmottakerModalBody.tsx
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+
+import { PlusCircleIcon } from '@navikt/aksel-icons';
+import { Alert, Button, Modal, VStack } from '@navikt/ds-react';
+
+import { RegistrerteBrevmottakere } from './oversikt/RegistrerteBrevmottakere';
+import { BrevmottakerForm } from './form/BrevmottakerForm';
+import { IPersonopplysninger } from '../../../../../App/typer/personopplysninger';
+import { Brevmottaker, finnesBrevmottakerMedMottakertype } from '../brevmottaker';
+import { Mottakertype } from '../mottakertype';
+import { OpprettBrevmottakerDto } from '../opprettBrevmottakerDto';
+
+type Props = {
+    behandlingId: string;
+    personopplysninger: IPersonopplysninger;
+    brevmottakere: Brevmottaker[];
+    opprettBrevmottaker: (opprettBrevmottakerDto: OpprettBrevmottakerDto) => void;
+    slettBrevmottaker: (brevmottakerId: string) => void;
+    erLesevisning: boolean;
+};
+
+export function BrevmottakerModalBody({
+    behandlingId,
+    personopplysninger,
+    brevmottakere,
+    opprettBrevmottaker,
+    slettBrevmottaker,
+    erLesevisning,
+}: Props) {
+    const [visForm, settVisForm] = useState(false);
+
+    const finnesBrevmottakerMedDødsbo = finnesBrevmottakerMedMottakertype(
+        brevmottakere,
+        Mottakertype.DØDSBO
+    );
+
+    const erFormSynlig =
+        !erLesevisning &&
+        !finnesBrevmottakerMedDødsbo &&
+        (brevmottakere.length === 0 || (brevmottakere.length === 1 && visForm));
+
+    const erLeggTilNyMottakerKnappSynlig =
+        !erLesevisning &&
+        !erFormSynlig &&
+        !finnesBrevmottakerMedDødsbo &&
+        brevmottakere.length === 1;
+
+    function lukkModal() {
+        settVisForm(false);
+    }
+
+    return (
+        <Modal.Body>
+            <VStack gap={'4'}>
+                <Alert variant={'info'}>
+                    Brev sendes til brukers folkeregistrerte adresse eller annen foretrukken kanal.
+                    Legg til mottaker dersom brev skal sendes til utenlandsk adresse, fullmektig,
+                    verge eller dødsbo.
+                </Alert>
+                <RegistrerteBrevmottakere
+                    brevmottakere={brevmottakere}
+                    slettBrevmottaker={slettBrevmottaker}
+                    erLesevisning={erLesevisning}
+                />
+                {erFormSynlig && (
+                    <BrevmottakerForm
+                        behandlingId={behandlingId}
+                        personopplysninger={personopplysninger}
+                        brevmottakere={brevmottakere}
+                        erLesevisning={false}
+                        lukkModal={lukkModal}
+                        opprettBrevmottaker={opprettBrevmottaker}
+                    />
+                )}
+                {erLeggTilNyMottakerKnappSynlig && (
+                    <div>
+                        <Button
+                            variant={'tertiary'}
+                            size={'small'}
+                            icon={<PlusCircleIcon />}
+                            onClick={() => settVisForm(true)}
+                        >
+                            Legg til ny mottaker
+                        </Button>
+                    </div>
+                )}
+            </VStack>
+        </Modal.Body>
+    );
+}
