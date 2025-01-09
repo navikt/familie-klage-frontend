75d2071fb1761e59127f4ef69d339362e916e306
thoalm <thoalm@outlook.com>
2025-01-09 10:44:18 +0100

    NAV-23444: Fjernet padding, bumpet aksel avhengighet

--- src/frontend/Komponenter/Behandling/Brevmottakere/baks/modal/BrevmottakerDetaljer.tsx
+++ src/frontend/Komponenter/Behandling/Brevmottakere/baks/modal/BrevmottakerDetaljer.tsx
@@ -19,7 +19,7 @@ export function BrevmottakerDetaljer({ brevmottaker, slettBrevmottaker, erLesevi
     const [visSlettFeilmelding, setVisSlettFeilmelding] = useState(false);
     const [laster, setLaster] = useState(false);
     return (
-        <Box padding={'2'}>
+        <Box>
             <VStack marginBlock={'2 2'} gap={'2'}>
                 {visSlettFeilmelding && (
                     <Alert
