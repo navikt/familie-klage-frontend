d4882135960a9ff2afec5d2dcbe80ef234cb79b0
thoalm <thoalm@outlook.com>
2025-01-15 15:26:12 +0100

    NAV-23919: Legger til RTL og vitest avhengigheter

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    plugins: [react()],
     test: {
         globals: true,
-        coverage: {
-            reporter: ['text', 'html'],
-        },
+        setupFiles: ['./vitest-setup.ts'],
+        environment: 'jsdom',
     },
 });
