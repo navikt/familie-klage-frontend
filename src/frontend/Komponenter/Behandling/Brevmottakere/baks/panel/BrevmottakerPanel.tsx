14d91dee33c6f2fdcdc7b100eefef60b5c9356fa
Thomas Alm <106926075+thoalm@users.noreply.github.com>
2025-02-13 10:28:05 +0100

    NAV-24253: Legger til en alert for å forklare hvorfor manuelle brevmottaker ikke er mulig å legge til (#899)

--- src/frontend/Komponenter/Behandling/Brevmottakere/baks/panel/BrevmottakerPanel.tsx
+++ src/frontend/Komponenter/Behandling/Brevmottakere/baks/panel/BrevmottakerPanel.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useApp } from '../../../../../App/context/AppContext';
 import { useBehandling } from '../../../../../App/context/BehandlingContext';
-import { BodyShort, Box, Button, HStack, Label, Tooltip } from '@navikt/ds-react';
+import { Alert, BodyShort, Box, Button, HStack, Label, Tooltip } from '@navikt/ds-react';
 import { utledOppsumertBrevmottakere } from '../oppsumertBrevmottaker';
 import { Brevmottakere } from '../../brevmottakere';
 import { useToggles } from '../../../../../App/context/TogglesContext';
@@ -19,27 +19,36 @@ export function BrevmottakerPanel({ brevmottakere }: Props) {
     const oppsumertBrevmottakere = utledOppsumertBrevmottakere(brevmottakere);
 
     return (
-        <Box background={'surface-info-subtle'} padding={'6'}>
-            <HStack justify={'space-between'} align={'center'}>
-                <Label htmlFor={'brevmottakere_liste'}>Brevmottakere</Label>
-                {behandlingErRedigerbar && toggles[ToggleName.leggTilBrevmottakerBaks] && (
-                    <Tooltip content={'Legg til eller fjern brevmottakere'}>
-                        <Button
-                            variant={'tertiary'}
-                            onClick={() => settVisBrevmottakereModal(true)}
-                        >
-                            Legg til/fjern brevmottakere
-                        </Button>
-                    </Tooltip>
-                )}
-            </HStack>
-            <ul id={'brevmottakere_liste'}>
-                {oppsumertBrevmottakere.map((oppsumertBrevmottaker) => (
-                    <li key={oppsumertBrevmottaker.id}>
-                        <BodyShort>{oppsumertBrevmottaker.visningstekst}</BodyShort>
-                    </li>
-                ))}
-            </ul>
-        </Box>
+        <>
+            {!toggles[ToggleName.leggTilBrevmottakerBaks] && (
+                <Alert variant={'warning'}>
+                    Muligheten for å legge til manuelle brevmottakere er foreløpig under utvikling
+                    og er dermed ikke tilgjengelig. Det er foreløpig kun mulig å sende brev til
+                    personen som er registrert på fagsaken.
+                </Alert>
+            )}
+            <Box background={'surface-info-subtle'} padding={'6'}>
+                <HStack justify={'space-between'} align={'center'}>
+                    <Label htmlFor={'brevmottakere_liste'}>Brevmottakere</Label>
+                    {behandlingErRedigerbar && toggles[ToggleName.leggTilBrevmottakerBaks] && (
+                        <Tooltip content={'Legg til eller fjern brevmottakere'}>
+                            <Button
+                                variant={'tertiary'}
+                                onClick={() => settVisBrevmottakereModal(true)}
+                            >
+                                Legg til/fjern brevmottakere
+                            </Button>
+                        </Tooltip>
+                    )}
+                </HStack>
+                <ul id={'brevmottakere_liste'}>
+                    {oppsumertBrevmottakere.map((oppsumertBrevmottaker) => (
+                        <li key={oppsumertBrevmottaker.id}>
+                            <BodyShort>{oppsumertBrevmottaker.visningstekst}</BodyShort>
+                        </li>
+                    ))}
+                </ul>
+            </Box>
+        </>
     );
 }
