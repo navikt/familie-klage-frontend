3dca0a794f31efd7c68fb4ab71f0287725fd311d
thoalm <thoalm@outlook.com>
2025-02-18 09:50:21 +0100

    NAV-23919: Renamer vitest setup fil

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     plugins: [react()],
     test: {
         globals: true,
-        setupFiles: ['./vitest-setup.ts'],
+        setupFiles: ['./vitest.setup.ts'],
         environment: 'jsdom',
     },
 });
