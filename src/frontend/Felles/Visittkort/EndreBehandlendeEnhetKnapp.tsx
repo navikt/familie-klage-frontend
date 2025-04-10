7f92bf5fd3cbc8803873087e020442c6d8d6b509
bragejahren <brage.ekroll.jahren@nav.no>
2025-04-10 14:46:51 +0200

    Legger funksjonalitet bak toggle

--- src/frontend/Felles/Visittkort/EndreBehandlendeEnhetKnapp.tsx
+++ src/frontend/Felles/Visittkort/EndreBehandlendeEnhetKnapp.tsx
@@ -2,6 +2,8 @@ import { useBehandling } from '../../App/context/BehandlingContext';
 import { Button } from '@navikt/ds-react';
 import React from 'react';
 import { Fagsystem } from '../../App/typer/fagsak';
+import { useToggles } from '../../App/context/TogglesContext';
+import { ToggleName } from '../../App/context/toggles';
 
 interface IProps {
     fagsystem: Fagsystem;
@@ -9,11 +11,13 @@ interface IProps {
 
 export const EndreBehandlendeEnhetKnapp: React.FC<IProps> = ({ fagsystem }: IProps) => {
     const { settVisEndreBehandlendeEnhet, behandlingErRedigerbar } = useBehandling();
+    const { toggles } = useToggles();
 
     const fagsystemerSomStøtterEndringAvBehandlendeEnhet = [Fagsystem.BA, Fagsystem.KS];
 
     const skalViseEndreBehandlendeEnhetKnapp =
-        fagsystemerSomStøtterEndringAvBehandlendeEnhet.includes(fagsystem);
+        fagsystemerSomStøtterEndringAvBehandlendeEnhet.includes(fagsystem) &&
+        toggles[ToggleName.skalKunneEndreBehandlendeEnhetBaks];
 
     return (
         skalViseEndreBehandlendeEnhetKnapp && (
