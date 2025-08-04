e66978ae3cf79445226d74e0d33532ea84125ec5
Thomas Alm <106926075+thoalm@users.noreply.github.com>
2025-08-04 14:15:14 +0200

    NAV-25804: Fjerner legg til brevmottaker toggle (#980)

--- src/frontend/App/context/toggles.ts
+++ src/frontend/App/context/toggles.ts
@@ -7,7 +7,6 @@ export enum ToggleName {
     skalKunneVelgePåklagetVedtakFraInfotrygd = 'familie.klage.infotrygd-vedtak',
 
     // Release-toggles
-    leggTilBrevmottakerBaks = 'familie-klage.legg-til-brevmottaker-baks',
     kanMellomlagreVurdering = 'familie-klage.kan-mellomlagre-vurdering',
     brukNyHenleggBehandlingModal = 'familie-klage.bruk-ny-henlegg-behandling-modal',
 }
