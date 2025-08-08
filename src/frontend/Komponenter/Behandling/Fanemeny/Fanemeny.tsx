24cbc35db70cdf3f7181acd1973c30797d8dbd51
Viktor Solberg <viktorgsolberg@gmail.com>
2025-08-08 09:28:05 +0200

    tatt i bruk css-modul for Fane-komponent

--- src/frontend/Komponenter/Behandling/Fanemeny/Fanemeny.tsx
+++ src/frontend/Komponenter/Behandling/Fanemeny/Fanemeny.tsx
@@ -1,45 +1,26 @@
 import * as React from 'react';
 import { FC } from 'react';
 import styles from './Fanemeny.module.css';
-import { alleSider, ISide, SideNavn } from './sider';
-import Fane from './Fane';
-import {
-    Behandling,
-    BehandlingResultat,
-    behandlingStegTilRekkefølge,
-    StegType,
-} from '../../../App/typer/fagsak';
-import { useBehandling } from '../../../App/context/BehandlingContext';
+import { alleSider } from './sider';
+import { Fane } from './Fane';
+import { Behandling } from '../../../App/typer/fagsak';
 
 interface Props {
     behandling: Behandling;
 }
 
-export const Fanemeny: FC<Props> = ({ behandling }) => {
-    const { formkravOppfylt } = useBehandling();
-    const faneErLåst = (side: ISide, steg: StegType): boolean => {
-        if (side.navn === SideNavn.VURDERING) {
-            return !formkravOppfylt;
-        }
-        if (side.navn === SideNavn.BREV && behandling.resultat === BehandlingResultat.HENLAGT) {
-            return true;
-        }
-        return side.rekkefølge > behandlingStegTilRekkefølge[steg];
-    };
-
-    return (
-        <div className={styles.stickyContainer}>
-            <div className={styles.fanemeny}>
-                {alleSider.map((side, index) => (
-                    <Fane
-                        side={side}
-                        behandlingId={behandling.id}
-                        index={index}
-                        deaktivert={faneErLåst(side, behandling.steg)}
-                        key={index}
-                    />
-                ))}
-            </div>
+export const Fanemeny: FC<Props> = ({ behandling }) => (
+    <div className={styles.stickyContainer}>
+        <div className={styles.fanemeny}>
+            {alleSider.map((side, index) => (
+                <Fane
+                    side={side}
+                    behandling={behandling}
+                    behandlingId={behandling.id}
+                    index={index}
+                    key={index}
+                />
+            ))}
         </div>
-    );
-};
+    </div>
+);
