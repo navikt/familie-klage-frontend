13c7ca3222ceac5473c14d1bbfa731e502f3483e
Viktor Solberg <viktorgsolberg@gmail.com>
2025-08-13 13:18:32 +0200

    faser ut familie-header fra fellesebibliotek

--- src/frontend/Felles/HeaderMedSøk/HeaderMedSøk.tsx
+++ src/frontend/Felles/HeaderMedSøk/HeaderMedSøk.tsx
@@ -1,15 +1,37 @@
 import React, { useMemo } from 'react';
-import { Header, PopoverItem } from '@navikt/familie-header';
 import { ISaksbehandler } from '../../App/typer/saksbehandler';
 import { useApp } from '../../App/context/AppContext';
 import styles from './HeaderMedSøk.module.css';
 import { AppEnv } from '../../App/api/env';
 import { lagAInntektLink, lagGosysLink, lagModiaLink } from '../Lenker/lenker';
 import { AxiosRequestCallback } from '../../App/typer/axiosRequest';
+import { ActionMenu, InternalHeader } from '@navikt/ds-react';
+import { MenuGridIcon } from '@navikt/aksel-icons';
 
-export interface IProps {
-    innloggetSaksbehandler: ISaksbehandler;
-}
+export const HeaderMedSøk: React.FunctionComponent<{ innloggetSaksbehandler: ISaksbehandler }> = ({
+    innloggetSaksbehandler,
+}) => {
+    const { axiosRequest, appEnv, valgtFagsakId, personIdent } = useApp();
+
+    const navn = innloggetSaksbehandler?.displayName ?? 'Ukjent';
+    const enhet = innloggetSaksbehandler?.enhet ?? 'Ukjent';
+    const popoverItems = [{ name: 'Logg ut', href: `${window.origin}/auth/logout` }];
+    const eksterneLenker = useMemo(
+        () => lagEksterneLenker(axiosRequest, appEnv, valgtFagsakId, personIdent),
+        [axiosRequest, appEnv, valgtFagsakId, personIdent]
+    );
+
+    return (
+        <div className={styles.stickyContainer}>
+            <Header
+                saksbehandlerNavn={navn}
+                saksbehandlerEnhet={enhet}
+                brukerPopoverItems={popoverItems}
+                eksterneLenker={eksterneLenker}
+            />
+        </div>
+    );
+};
 
 const lagAInntekt = (
     axiosRequest: AxiosRequestCallback,
@@ -65,29 +87,110 @@ const lagEksterneLenker = (
     lagModia(appEnv, personIdent),
 ];
 
-export const HeaderMedSøk: React.FunctionComponent<IProps> = ({ innloggetSaksbehandler }) => {
-    const { axiosRequest, gåTilUrl, appEnv, valgtFagsakId, personIdent } = useApp();
+export type PopoverItem =
+    | {
+          name: string;
+          href: string;
+          isExternal?: boolean;
+          onSelect?: never;
+      }
+    | {
+          name: string;
+          href?: never;
+          isExternal?: never;
+          onSelect: (e: Event) => void;
+      };
 
-    const eksterneLenker = useMemo(
-        () => lagEksterneLenker(axiosRequest, appEnv, valgtFagsakId, personIdent),
-        [axiosRequest, appEnv, valgtFagsakId, personIdent]
-    );
+interface Props {
+    saksbehandlerNavn: string;
+    saksbehandlerEnhet: string;
+    brukerPopoverItems: PopoverItem[];
+    eksterneLenker: PopoverItem[];
+}
+
+export const Header: React.FC<Props> = ({
+    saksbehandlerNavn,
+    saksbehandlerEnhet,
+    brukerPopoverItems,
+    eksterneLenker,
+}) => {
+    const { gåTilUrl } = useApp();
 
     return (
-        <div className={styles.stickyContainer}>
-            <Header
-                tittelOnClick={() => {
-                    gåTilUrl('#');
-                }}
-                tittelHref={'#'}
-                tittel="NAV Familie - klage"
-                brukerinfo={{
-                    navn: innloggetSaksbehandler?.displayName || 'Ukjent',
-                    enhet: innloggetSaksbehandler?.enhet,
-                }}
-                brukerPopoverItems={[{ name: 'Logg ut', href: `${window.origin}/auth/logout` }]}
-                eksterneLenker={eksterneLenker}
+        <InternalHeader>
+            <InternalHeader.Title className={styles.header} onClick={() => gåTilUrl('#')}>
+                NAV Familie - klage
+            </InternalHeader.Title>
+            <LenkePopover lenker={eksterneLenker} />
+            <Bruker
+                navn={saksbehandlerNavn}
+                enhet={saksbehandlerEnhet}
+                popoverItems={brukerPopoverItems}
             />
-        </div>
+        </InternalHeader>
     );
 };
+
+const Bruker: React.FC<{
+    navn: string;
+    enhet?: string;
+    popoverItems?: PopoverItem[];
+}> = ({ navn, enhet, popoverItems }) => (
+    <ActionMenu>
+        <ActionMenu.Trigger>
+            <InternalHeader.UserButton
+                name={navn}
+                description={enhet ? `Enhet: ${enhet}` : 'Ukjent enhet'}
+                className="ml-auto"
+            />
+        </ActionMenu.Trigger>
+        {popoverItems && (
+            <ActionMenu.Content>
+                <ActionMenu.Group label={''}>
+                    {popoverItems.map((lenke, index) => {
+                        return <ActionMenuLenke key={index} lenke={lenke} />;
+                    })}
+                </ActionMenu.Group>
+            </ActionMenu.Content>
+        )}
+    </ActionMenu>
+);
+
+const LenkePopover: React.FC<{ lenker: PopoverItem[] }> = ({ lenker }) => (
+    <ActionMenu>
+        <ActionMenuTrigger />
+        {lenker.length > 0 && (
+            <ActionMenu.Content>
+                <ActionMenu.Group label={''}>
+                    {lenker.map((lenke, index) => (
+                        <ActionMenuLenke lenke={lenke} key={index} />
+                    ))}
+                </ActionMenu.Group>
+            </ActionMenu.Content>
+        )}
+    </ActionMenu>
+);
+
+const ActionMenuLenke: React.FC<{
+    lenke: PopoverItem;
+}> = ({ lenke }) =>
+    lenke.onSelect ? (
+        <ActionMenu.Item onSelect={(e) => lenke.onSelect(e)}>{lenke.name}</ActionMenu.Item>
+    ) : (
+        <ActionMenu.Item
+            as={'a'}
+            href={lenke.href}
+            target={lenke.isExternal ? '_blank' : ''}
+            rel={lenke.isExternal ? 'noopener noreferrer' : ''}
+        >
+            {lenke.name}
+        </ActionMenu.Item>
+    );
+
+const ActionMenuTrigger = () => (
+    <ActionMenu.Trigger>
+        <InternalHeader.Button className="ml-auto">
+            <MenuGridIcon fontSize={'1.5rem'} title="Andre systemer" />
+        </InternalHeader.Button>
+    </ActionMenu.Trigger>
+);
