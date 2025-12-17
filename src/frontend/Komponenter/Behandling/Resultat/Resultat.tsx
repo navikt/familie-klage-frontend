2baca4725fa55707f75604e1be2f7f536ac996cb
endrehm <endre.heien.mo@nav.no>
2025-12-17 11:05:54 +0100

    Lagt til ny event type(GJENOPPTAKSBEHANDLING_AVSLUTTET) sammen med tilhørende utfall. Har også lagt til varsel for nytt event, trengs muligens ikke

--- src/frontend/Komponenter/Behandling/Resultat/Resultat.tsx
+++ src/frontend/Komponenter/Behandling/Resultat/Resultat.tsx
@@ -7,6 +7,7 @@ import { Heading } from '@navikt/ds-react';
 import { useBehandling } from '../../../App/context/BehandlingContext';
 import { AnkeVisning } from './AnkeVisning';
 import { FeilregistrertVisning } from './FeilregistrertVisning';
+import { GjenoptakVisning } from './GjenoptakVisning';
 
 export const Resultat: React.FC = () => {
     const { behandling, hentBehandling, behandlingHistorikk } = useBehandling();
@@ -25,6 +26,7 @@ export const Resultat: React.FC = () => {
                         </Heading>
                         <FeilregistrertVisning behandling={behandling} />
                         <AnkeVisning behandling={behandling} />
+                        <GjenoptakVisning behandling={behandling} />
                     </div>
                     <div className={styles.tidslinjeContainer}>
                         <Tidslinje
