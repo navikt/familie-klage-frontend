c2e5a633a7deca95bee5100507268c08e1720dfb
Magnus Tønnessen <Magnus.Tonnessen@nav.no>
2026-02-02 16:25:14 +0100

    Legg til hjelpefunksjoner

--- src/frontend/Komponenter/Behandling/Brevmottakere/brevmottakere.ts
+++ src/frontend/Komponenter/Behandling/Brevmottakere/brevmottakere.ts
@@ -1,6 +1,22 @@
-import { BrevmottakerOrganisasjon, BrevmottakerPerson } from './brevmottaker';
+import {
+    Brevmottaker,
+    BrevmottakerOrganisasjon,
+    BrevmottakerPerson,
+    BrevmottakerPersonUtenIdent,
+    erBrevmottakerPersonUtenIdent,
+} from './brevmottaker';
 
 export interface Brevmottakere {
     personer: BrevmottakerPerson[];
     organisasjoner: BrevmottakerOrganisasjon[];
 }
+
+export const hentBrevmottakerPersonUtenIdenter = (
+    brevmottakere: Brevmottakere
+): BrevmottakerPersonUtenIdent[] =>
+    brevmottakere.personer.filter((brevmottaker) => erBrevmottakerPersonUtenIdent(brevmottaker));
+
+export const hentAlleBrevmottakereSomListe = (brevmottakere: Brevmottakere): Brevmottaker[] => [
+    ...brevmottakere.personer,
+    ...brevmottakere.organisasjoner,
+];
