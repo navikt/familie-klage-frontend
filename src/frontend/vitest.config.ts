ab9d3dbd149e2a378ab29c9ada8785a53c89f8d1
Thomas Alm <106926075+thoalm@users.noreply.github.com>
2026-08-31 11:37:00 +0200

    NAV-30438: Deler opp vitest i backend og frontend (#1267)

--- src/frontend/vitest.config.ts
+++ src/frontend/vitest.config.ts
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+    root: import.meta.dirname,
     plugins: [react()],
     test: {
         globals: true,
